d220992fb3cf81dee030e1ffc6bcd6e3d780c0f2
xiageee <qin90@qq.com>
2021-12-11 20:34:10 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/xiage/wiki/controller/TestController.java
+++ src/main/java/com/xiage/wiki/controller/TestController.java
@@ -1,14 +1,31 @@
 package com.xiage.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-//    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    @RequestMapping("/hello")
-    public String hello(){
+    /**
+     * GET, POST, PUT, DELETE
+     *
+     * /user?id=1
+     * /user/1
+     * @return
+     */
+    // @PostMapping
+    // @PutMapping
+    // @DeleteMapping
+    // @RequestMapping(value = "/user/1", method = RequestMethod.GET)
+    // @RequestMapping(value = "/user/1", method = RequestMethod.DELETE)
+    @GetMapping("/hello")
+    public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
     }
 }
