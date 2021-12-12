fe4459396edd1fa9e1b0bcbaf429c1b7836ac9fe
xiageee <qin90@qq.com>
2021-12-12 22:43:02 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/xiage/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.xiage.wiki.service;
+
+import com.xiage.wiki.domain.Ebook;
+import com.xiage.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+}
