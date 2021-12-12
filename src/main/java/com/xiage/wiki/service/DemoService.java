aeae64a9487da93351fa15b3f2a3998314efa294
xiageee <qin90@qq.com>
2021-12-12 22:03:07 +0800

    3-5 增加了demo表,演示列表查询(通过generator)

---
+++ src/main/java/com/xiage/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.xiage.wiki.service;
+
+import com.xiage.wiki.domain.Demo;
+import com.xiage.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
