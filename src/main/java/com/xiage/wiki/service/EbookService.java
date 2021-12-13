e19de88ba1f6ffce927e656cdd7dd6d489f52b3c
xiageee <qin90@qq.com>
2021-12-13 21:46:41 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/xiage/wiki/service/EbookService.java
+++ src/main/java/com/xiage/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.xiage.wiki.service;
 
 import com.xiage.wiki.domain.Ebook;
+import com.xiage.wiki.domain.EbookExample;
 import com.xiage.wiki.mapper.EbookMapper;
+import com.xiage.wiki.req.EbookReq;
+import com.xiage.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,20 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        /** 相当于创建where条件 */
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        /** 遍历ebookList,将结果转成List<EbookResp> */
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
