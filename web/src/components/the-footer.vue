c4433c83bc1bab99a75d5d707c2b9190e31a1396
xiageee <qin90@qq.com>
2021-12-14 22:48:55 +0800

    4-7 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        虾哥电子书
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+
+    export default defineComponent({
+        name: 'the-footer',
+    });
+</script>
