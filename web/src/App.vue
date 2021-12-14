31e353b5cc31ba41fb8707b4b098d2298c1eaa42
xiageee <qin90@qq.com>
2021-12-14 22:43:35 +0800

    4-7 制作the-header组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,23 @@
 <template>
     <a-layout>
-        <a-layout-header class="header">
-            <div class="logo" />
-            <a-menu
-                    theme="dark"
-                    mode="horizontal"
-                    v-model:selectedKeys="selectedKeys1"
-                    :style="{ lineHeight: '64px' }"
-            >
-                <a-menu-item key="1">nav 1</a-menu-item>
-                <a-menu-item key="2">nav 2</a-menu-item>
-                <a-menu-item key="3">nav 3</a-menu-item>
-            </a-menu>
-        </a-layout-header>
+        <the-header></the-header>
         <router-view/>
         <a-layout-footer style="text-align: center">
             虾哥电子书
         </a-layout-footer>
     </a-layout>
 </template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import TheHeader from '@/components/the-header.vue';
+    export default defineComponent({
+        name: 'app',
+        components:{
+            TheHeader,
+        },
+    });
+</script>
 
 <!--css样式-->
 <style>
