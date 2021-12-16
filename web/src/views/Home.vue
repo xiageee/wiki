8248ba5414f5b0d12b2b200dc3c6aea3c8adcf4c
xiageee <qin90@qq.com>
2021-12-16 16:31:51 +0800

    5-5 修改axios的baseURL读配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -93,8 +93,8 @@
             const ebooks = ref();
             const ebooks1 = reactive({books: []});
             onMounted(() => {
-                console.log("onMounted111")
-                axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
+                console.log("onMounted222")
+                axios.get("/ebook/list").then((response) => {
                     const data = response.data;
                     ebooks.value = data.content;
                     ebooks1.books = data.content;
