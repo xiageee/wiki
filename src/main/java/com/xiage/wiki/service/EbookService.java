2c113c30ce9993a6d30ecf98ba6f9af717645727
xiageee <qin90@qq.com>
2021-12-17 00:11:48 +0800

    6-4 使用PageHelper实现后端分页

--- src/main/java/com/xiage/wiki/service/EbookService.java
+++ src/main/java/com/xiage/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.xiage.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.xiage.wiki.domain.Ebook;
 import com.xiage.wiki.domain.EbookExample;
 import com.xiage.wiki.mapper.EbookMapper;
 import com.xiage.wiki.req.EbookReq;
 import com.xiage.wiki.resp.EbookResp;
 import com.xiage.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,6 +18,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -25,7 +31,13 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数: {}",pageInfo.getTotal());
+        LOG.info("总页数: {}",pageInfo.getPages());
+
 
         /** 遍历ebookList,将结果转成List<EbookResp> */
         //List<EbookResp> respList = new ArrayList<>();
