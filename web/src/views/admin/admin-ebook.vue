443c03d0ae6a8938b9458e660c30ac763ce3e056
xiageee <qin90@qq.com>
2021-12-19 13:26:25 +0800

    6-10 集成validation为列表接口增加参数校验

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -68,6 +68,7 @@
 <script lang="ts">
     import { defineComponent, onMounted, ref } from 'vue';
     import axios from 'axios';
+    import { message } from "ant-design-vue";
 
     export default defineComponent({
         name: 'AdminEbook',
@@ -75,7 +76,7 @@
             const ebooks = ref();
             const pagination = ref({
                 current: 1,
-                pageSize: 4,
+                pageSize: 1001,
                 total: 0
             });
             const loading = ref(false);
@@ -130,11 +131,16 @@
                 }).then((response) => {
                     loading.value = false;
                     const data = response.data;
-                    ebooks.value = data.content.list;
+                    if (data.success){
+                        ebooks.value = data.content.list;
 
-                    //重置分页按钮
-                    pagination.value.current = params.page;
-                    pagination.value.total = data.content.total;
+                        //重置分页按钮
+                        pagination.value.current = params.page;
+                        pagination.value.total = data.content.total;
+                    } else {
+                        //弹出错误提示框
+                        message.error(data.message);
+                    }
                 });
             };
 
